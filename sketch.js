9edf38deb5befa2d493d44995df3d9382bd3656f
Akshat <71318567+Akshat320@users.noreply.github.com>
2020-11-30 17:40:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ball;
+var database, position;
+var ballPosition;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+    console.log(database);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    ballPosition = database.ref("position");
+    ballPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y)
+{
+   
+       database.ref("position").set({
+      'x': position.x + x,
+      'y': position.y + y
+
+  });
+}
+
+function showError()
+{
+    console.log("error in writing to the database");
+}
+
+function readPosition(data)
+{
+
+    position = data.val();
+    console.log(position);
+    ball.x = position.x;
+    ball.y = position.y;
+}
